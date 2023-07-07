79c9626e1a88355d9ca30e8a425e61ce7befc794
laura.vlasma <laura.vlasma@AWI22NB05>
2023-07-07 15:32:38 +0200

    adding-fades

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,30 +2,32 @@ import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons"
 import { faAt } from "@fortawesome/free-solid-svg-icons"
+import { Fade } from "react-awesome-reveal";
 
 const Contact = () => {
   return (
-
-    <section id='contact' className='contact container'>
-    <div className='column-item'>
-      <div className='column-item__content'>
-          <h3 className='subtitle'>04. What's next?</h3>
-          <h2>Job for me</h2>
-          <p>Send me a message on LinkedIn or through email to get in touch! I am always open to chat about building fun projects.</p>
-          <div className='d-flex gap-3 mt-3'>
-            <a className='cbtn cbtn--br2' href='https://www.linkedin.com/in/laura-vlasma-0692b0159/' target='_blank' rel="noreferrer">
-              <FontAwesomeIcon icon={faLinkedinIn} />
-            </a>
-            <a className='cbtn cbtn--br1' href='mailto:lauravlasma1@gmail.com'>
-              <FontAwesomeIcon icon={faAt} />
-            </a>
-          </div>     
-      </div>
-      <div className='column-item__image'>
-      <img src='assets/workdesk.svg' alt='design' />
-      </div>
-    </div>   
-  </section>
+    <Fade>
+      <section id='contact' className='contact container'>
+      <div className='column-item'>
+        <div className='column-item__content'>
+            <h3 className='subtitle'>04. What's next?</h3>
+            <h2>Job for me</h2>
+            <p>Send me a message on LinkedIn or through email to get in touch! I am always open to chat about building fun projects.</p>
+            <div className='d-flex gap-3 mt-3'>
+              <a className='cbtn cbtn--br2' href='https://www.linkedin.com/in/laura-vlasma-0692b0159/' target='_blank' rel="noreferrer">
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </a>
+              <a className='cbtn cbtn--br1' href='mailto:lauravlasma1@gmail.com'>
+                <FontAwesomeIcon icon={faAt} />
+              </a>
+            </div>     
+        </div>
+        <div className='column-item__image'>
+        <img src='assets/workdesk.svg' alt='design' />
+        </div>
+      </div>   
+    </section>
+  </Fade>
   )
 }
 
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { projects } from '../data';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons"
+import { Fade } from "react-awesome-reveal";
 
 export default function Work () {
   return (
@@ -12,31 +13,33 @@ export default function Work () {
       </header>
       <div className='work__items'>
         {projects.map((project, index) => (
-          <article className='project' key={index}>
-            <div className='project__content'>
-              <div className='description'>
-                <div className='description__header'>
-                  <a href={project.url} target='_blank' rel='noreferrer'>
-                    <h3>{project.title}</h3>
-                    <FontAwesomeIcon title='External Link' icon={faArrowUpRightFromSquare} />
-                  </a>
+          <Fade>
+            <article className='project' key={index}>
+              <div className='project__content'>
+                <div className='description'>
+                  <div className='description__header'>
+                    <a href={project.url} target='_blank' rel='noreferrer'>
+                      <h3>{project.title}</h3>
+                      <FontAwesomeIcon title='External Link' icon={faArrowUpRightFromSquare} />
+                    </a>
+                  </div>
+                  <p>{project.story}</p>
+                  <div className='tools'>
+                  {
+                  project.tools.map((tool, index) => {
+                    return (
+                      <span key={index}>{tool}</span>
+                      )
+                    })
+                  }
+                  </div>     
                 </div>
-                <p>{project.story}</p>
-                <div className='tools'>
-                {
-                project.tools.map((tool, index) => {
-                  return (
-                    <span key={index}>{tool}</span>
-                    )
-                  })
-                }
-                </div>     
               </div>
-            </div>
-            <a className='project__img' href={project.url} target='_blank' rel='noreferrer'>
-              <img src={project.img} alt={project.title} />
-            </a>
-          </article>
+              <a className='project__img' href={project.url} target='_blank' rel='noreferrer'>
+                <img src={project.img} alt={project.title} />
+              </a>
+            </article>
+          </Fade>
         ))}
       </div>
     </section>
