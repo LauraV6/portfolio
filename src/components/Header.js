6571e44253f446343196ea25210593b78650464f
laura.vlasma <laura.vlasma@AWI22NB05>
2023-07-07 16:41:48 +0200

    actual-nav-fade-fix-oops

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,19 +21,19 @@ const Header = () => {
   return (
       <header id='home' className='container'>
         <Navigation />
-          <div className='header'>
           <Fade>
-            <div className='header__content'>
-                <h3 ref={heroSubTitleRef} className='subtitle subtitle--odd'>01. Laura Vlasma</h3>
-                <h1 ref={heroTitleRef}>All-round (multimedia) <span className='notable'>Designer</span> and <span className='notable'>Front-end Developer</span></h1>
-                <p ref={heroTextRef}>I’m a Frontend Developer specializing in designing and building exceptional digital experiences. Currently, I’m focused on building accessible, human-centered applications at <a className='hyperlink' href='https://www.awisoftware.nl' target='_blank' rel='noreferrer'>AWI Software.</a></p>
-                <a ref={heroButtonRef} href='#work' className='cbtn cbtn--br1'>Recent work</a>
-            </div>
-            <div className='header__image'>
-              {[...Array(logoAmount)].map((e, i) => <span className={'logo logo-' + i} key={i}>L.</span>)}       
+            <div className='header'>
+              <div className='header__content'>
+                  <h3 ref={heroSubTitleRef} className='subtitle subtitle--odd'>01. Laura Vlasma</h3>
+                  <h1 ref={heroTitleRef}>All-round (multimedia) <span className='notable'>Designer</span> and <span className='notable'>Front-end Developer</span></h1>
+                  <p ref={heroTextRef}>I’m a Frontend Developer specializing in designing and building exceptional digital experiences. Currently, I’m focused on building accessible, human-centered applications at <a className='hyperlink' href='https://www.awisoftware.nl' target='_blank' rel='noreferrer'>AWI Software.</a></p>
+                  <a ref={heroButtonRef} href='#work' className='cbtn cbtn--br1'>Recent work</a>
+              </div>
+              <div className='header__image'>
+                {[...Array(logoAmount)].map((e, i) => <span className={'logo logo-' + i} key={i}>L.</span>)}       
+              </div>
             </div>
             </Fade>
-          </div>
           <Intro />
       </header>
   )
